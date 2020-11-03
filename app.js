ae2de96776b97245115ea46d0b057de5632b77fa
Niko Bec <nikobec96@gmail.com>
2020-11-03 12:52:27 +0100

    logic to add new list item + comments

--- app.js
+++ app.js
@@ -3,23 +3,45 @@ const app = express();
 const https = require("https");
 const bodyParser = require("body-parser");
 
+//Array of new items
+var items =[];
+
+//Initializing the ejs module
 app.set("view engine", "ejs");
 
-app.get("/", function (req, res) {
-  var day = "";
-  var date = new Date();
-  var currentDay = date.getDay();
+//Initializing the body parser module
+app.use(bodyParser.urlencoded({extended:true}));
 
-  if (currentDay === 6 || currentDay === 0) {
-    day = "the weekend!";
-    //res.send("It's the weekend!");
-  } else {
-    day = "a workday!";
-    res.render('list', {today: day});
-  }
+app.get("/", function (req, res) {
+  
+  //Options for the date
+    var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  // New instance of the class Date
+  var date = new Date();
+
+  // function to get the date in english with specific options
+  var day = date.toLocaleDateString("en-UK", options);
+
+  res.render("list", {today: day, newitem: items});
+
 });
 
-app.post("/", function (req, res) {});
+app.post("/", function (req, res) {
+
+    // get value from input/new item
+    var item = req.body.newItem;
+    
+    //append new item into array of items
+    items.push(item);
+
+    //redirect to home route
+    res.redirect("/");
+});
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
