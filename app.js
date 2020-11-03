35f2a57da4253998cad072291da61e15f536fa20
Niko Bec <nikobec96@gmail.com>
2020-11-03 17:50:30 +0100

    added a seperate work list

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ const https = require("https");
 const bodyParser = require("body-parser");
 
 //Array of new items
-var items =[];
+var items = [];
+var workitems = [];
 
 //Initializing the ejs module
 app.set("view engine", "ejs");
 
 //Initializing the body parser module
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  
   //Options for the date
-    var options = {
+  var options = {
     weekday: "long",
     day: "numeric",
     month: "long",
@@ -29,20 +29,35 @@ app.get("/", function (req, res) {
   // function to get the date in english with specific options
   var day = date.toLocaleDateString("en-UK", options);
 
-  res.render("list", {today: day, newitem: items});
-
+  res.render("list", { listTitle: day, newitem: items });
 });
 
 app.post("/", function (req, res) {
 
-    // get value from input/new item
-    var item = req.body.newItem;
-    
+  // get value from input/new item
+  var item = req.body.newItem;
+
+  if (req.body.list === "Work List") {
+    workitems.push(item);
+    res.redirect("/work");
+
+  } else {
     //append new item into array of items
     items.push(item);
 
     //redirect to home route
     res.redirect("/");
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newitem: workitems });
+});
+
+app.post("/work", function (req, res) {
+  let item = req.body.newitem;
+  workitems.push(item);
+  res.redirect("/work");
 });
 
 app.listen(3000, function () {
