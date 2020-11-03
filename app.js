317d8e12f48d3f741cd2b0aee28df299e94c8430
Niko Bec <nikobec96@gmail.com>
2020-11-03 11:21:06 +0100

    added ejs file and tested it

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ const app = express();
 const https = require("https");
 const bodyParser = require("body-parser");
 
-app.get("/", function(req,res){
-    console.log("Hello world");
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  var day = "";
+  var date = new Date();
+  var currentDay = date.getDay();
+
+  if (currentDay === 6 || currentDay === 0) {
+    day = "the weekend!";
+    //res.send("It's the weekend!");
+  } else {
+    day = "a workday!";
+    res.render('list', {today: day});
+  }
 });
 
-app.post("/", function(){
-    
-});
+app.post("/", function (req, res) {});
 
-app.listen(3000, function(){
-    console.log("Server is running on port 3000");
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
 });
